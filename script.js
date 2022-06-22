831cb8a75cb3b25a562f76a7be579e0694a19faf
wj-tan <1901864@sit.singaporetech.edu.sg>
2022-06-22 17:13:36 +0800

    Created force simulation

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ let width = 800, height = 800;
 let svg = d3.select("svg")
     .attr("viewBox", "0 0 " + width + " " + height)
 
-let xPosition = d3.scaleOrdinal()
-    .domain([0, 1, 2])
-    .range([150, 400, 650]);
-
 // Load external data
 Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(data => {
 
@@ -44,11 +40,14 @@ Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(d
         .range(["steelblue", "pink"]);
 
 
+
+
+
     // Add in circles
     let node = svg.append("g")
         .attr("id", "nodes")
         .selectAll("circle")
-        .data(data)
+        .data(data[1])
         .enter()
         .append("circle")
         .attr("r", 10)
@@ -60,16 +59,42 @@ Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(d
             .on("end", dragended));
 
 
+    // Link Strcuture
+    let xPosition = d3.scaleOrdinal()
+        .domain([0, 1, 2])
+        .range([150, 650]);
+
+    let links = [];
+
+    let linkpath = svg.append("g")
+        .attr("id", "links")
+        .selectAll("path")
+        .data(links)
+        .enter()
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke", "black");
+
+
     let simulation = d3.forceSimulation()
-        .nodes(data)
+        .nodes(data[1])
         .force("x", d3.forceX().strength(0.5).x(d => xPosition(d.class)))
         .force("y", d3.forceY().strength(0.1).y(height / 2))
         // .force("link", d3.forceLink(links).id(d => d.id))
-        .force("link", d3.forceLink(links)
+        .force("link", d3.forceLink(data[0])
             .id(d => d.id)
             .distance(50)
             .strength(0.5))
         .force("charge", d3.forceManyBody().strength(-15)) // Charge Determines whether each nodes attract or reject each other
         .force("collide", d3.forceCollide().strength(0.1).radius(15))
+
+        .on("tick", d => {
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+
+            linkpath
+                .attr("d", d => "M" + d.source.x + "," + d.source.y + " " + d.target.x + "," + d.target.y);
+        });
 
 })
